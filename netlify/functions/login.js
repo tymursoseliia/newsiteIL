3b35806c05a18f8a13c9fb4f2f76282d7096e7fc
gorbacenkovitalij6-prog <tymursoseliia@gmail.com>
2026-07-10 15:04:05 +0300

    Configure Netlify login serverless function

---
+++ netlify/functions/login.js
@@ -0,0 +1,52 @@
+const ADMIN_PASSWORD = 'admin123';
+const ADMIN_TOKEN = 'secret_admin_token_9823472';
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      }
+    };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: 'Method Not Allowed' })
+    };
+  }
+
+  try {
+    const { password } = JSON.parse(event.body);
+    if (password === ADMIN_PASSWORD) {
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({ token: ADMIN_TOKEN })
+      };
+    } else {
+      return {
+        statusCode: 401,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({ error: 'Неверный пароль!' })
+      };
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
